6c3a65c40f7a3fe6d8738cdeab28179ed458dfed
wbin <447877752@qq.com>
2016-11-19 00:56:35 +0800

    init web project

---
+++ DBconfig.js
@@ -0,0 +1,7 @@
+module.exports = {
+	user: 'postgres',
+	password: '123456',
+	database: 'hackathon',
+	host: 'localhost',
+	port: 5432
+};
---
+++ app_api/routes/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by wbin on 2016/11/19.
+ */
+var express = require('express');
+var router = express.Router();
+
+
+module.exports = router;
